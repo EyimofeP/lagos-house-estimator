60b9cff1cac918ba01cb5de132966fa52fd21fd2
Eyimofe <eyimofe36@gmail.com>
2022-09-21 03:18:07 +0100

    added rent files to backend

--- web-app/predict/urls.py
+++ web-app/predict/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("predict/", views.predict, name="predict"),
+    path("buy/", views.buy, name="buy"),
+    path("predict-buy/", views.predict_buy, name="predict-buy"),
+    path("rent/", views.rent, name="rent"),
+    path("predict-rent/", views.predict_rent, name="predict-rent"),
 ]
--- web-app/predict/views.py
+++ web-app/predict/views.py
@@ -6,9 +6,12 @@ import joblib
 def home(request):
     return render(request, "base.html")
 
+# Buy page
+def buy(request):
+    return render(request, "buy.html")
 
-# Predict page
-def predict(request):
+# predict buy
+def predict_buy(request):
     # Fetch data from the frontend
     bedrooms = float(request.POST["Bedrooms"])
     bathrooms = float(request.POST["Bathrooms"])
@@ -46,7 +49,7 @@ def predict(request):
         test[numerical] = scaler.transform(test[numerical]) # scale the numerical data
         test = encoder.transform(test) # encode the categorical data
         
-        return model.predict(test)[0] // 1e6 # make prediction, convert it to millions and round up to 2dp 
+        return model.predict(test)
 
     # Getting prediction and convert to integer
     prediction = int(predict_house(data))
@@ -55,12 +58,58 @@ def predict(request):
     context = {"prediction" : prediction,
                 "Bedrooms" : int(bedrooms),
                 "Bathrooms" : int(bathrooms),
-                "Toilets" : int(toilets),
-                "Newly Built" : newly_built,
-                "Serviced" : serviced,
-                "Furnished" : furrnished,
-                "Type" : type_of,
-                "City" : city,
-                "Neighborhood" : neighborhood}
+                "Toilets" : int(toilets)}
     
-    return render(request, "base.html", context=context)
+    return render(request, "buy.html", context=context)
+
+# rent page
+def rent(request):
+    return render(request, "rent.html")
+
+# predict rent
+def predict_rent(request):
+    # Fetch data from the frontend
+    bedrooms = float(request.POST["Bedrooms"])
+    bathrooms = float(request.POST["Bathrooms"])
+    toilets = float(request.POST["Toilets"])
+    newly_built = float(request.POST["Newly_Built"])
+    serviced = float(request.POST["Serviced"])
+    furrnished = float(request.POST["Furnished"])
+    type_of = request.POST["Type"]
+    city = request.POST["City"]
+    neighborhood = request.POST["Neighborhood"]
+
+    # Store data in a dictionary
+    data = {
+    "Bedrooms" : bedrooms,
+    "Bathrooms" : bathrooms,
+    "Toilets" : toilets,
+    "Newly Built" : newly_built,
+    "Serviced" : serviced,
+    "Furnished" : furrnished,
+    "Property Type" : type_of,
+    "City" : city,
+    "Neighborhood" : neighborhood}
+
+    # Import tools for preprocessing
+    encoder = joblib.load("tools/rent_encoder_joblib")
+
+    # Import model
+    model = joblib.load("tools/rent_model_joblib")    
+
+    # Function to predict price
+    def predict_house(data):
+        test = pd.DataFrame([data])
+        test = encoder.transform(test) # encode the categorical data
+        return model.predict(test)[0]
+
+    # Getting prediction and convert to integer
+    prediction = int(predict_house(data))
+
+    # Send to frontend
+    context = {"prediction" : prediction,
+                "Bedrooms" : int(bedrooms),
+                "Bathrooms" : int(bathrooms),
+                "Toilets" : int(toilets)}
+    
+    return render(request, "rent.html", context=context)
